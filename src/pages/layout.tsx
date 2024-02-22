36e6a3441dadc375ab6057a75967e9162e759ff4
Ryan Toronto <ryanto@gmail.com>
2024-02-22 16:09:13 -0500

    wip

--- src/pages/layout.tsx
+++ src/pages/layout.tsx
@@ -6,11 +6,13 @@ export default function Layout({ children }: { children: ReactNode }) {
   return (
     <html>
       <head>
-        <title>Twofold RSC</title>
+        <title>Books</title>
         <link rel="icon" href="data:;base64,iVBORw0KGgo=" />
+        <meta charSet="utf-8" />
+        <meta name="Content-Type" content="text/html; charset=utf-8" />
       </head>
       <body>
-        <main className="mx-auto max-w-7xl px-4 py-8">{children}</main>
+        <main className="">{children}</main>
       </body>
 
       {/* This component is needed to start Twofold */}
